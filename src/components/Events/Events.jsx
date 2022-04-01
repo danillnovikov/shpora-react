621abbd99c1720baf02a9108e9cfd5849261ca36
Daniil <danill.novikov@mail.ru>
2022-04-02 02:24:43 +0300

    add info lifecycle and events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -9,13 +9,119 @@ const Events = () => {
     <Layout className="layout">
       <Content style={{ padding: '0 87px' }}>
         <div className="site-layout-content">
-          <h1 className="title">События</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Perspiciatis mollitia incidunt placeat saepe reprehenderit.
-            Similique rerum repellat, unde vero porro, atque magnam quia fugit
-            ad esse nostrum. Nihil, veniam architecto.
+          <h1 className="title">Обработка событий</h1>
+          <p className="component_text">
+            Обработка событий в React-элементах очень похожа на обработку
+            событий в DOM-элементах. Но есть несколько синтаксических отличий:
           </p>
+          <ul className="component_text">
+            <li>
+              События в React именуются в стиле camelCase вместо нижнего
+              регистра.
+            </li>
+            <li>
+              С JSX вы передаёте функцию как обработчик события вместо строки.
+            </li>
+          </ul>
+          <p className="component_text">Например, в HTML:</p>
+          <div className="code">
+            <pre>
+              <code>{`<button onclick="activateLasers()">
+  Активировать лазеры
+</button>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">В React немного иначе:</p>
+          <div className="code">
+            <pre>
+              <code>{`<button onClick={activateLasers}>
+  Активировать лазеры
+</button>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            Ещё одно отличие — в React нельзя предотвратить обработчик события
+            по умолчанию, вернув false. Нужно явно вызвать preventDefault.
+            Например, в обычном HTML для отмены отправки формы (действие по
+            умолчанию) можно написать:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`<form onsubmit="console.log('Отправлена форма.'); return false">
+  <button type="submit">Отправить</button>
+</form>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">В React это будет выглядеть так:</p>
+          <div className="code">
+            <pre>
+              <code>{`function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Отправлена форма.');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Отправить</button>
+    </form>
+  );
+}`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            В приведённом выше коде e — это синтетическое событие. React
+            определяет синтетические события в соответствии{' '}
+            <a href="https://www.w3.org/TR/DOM-Level-3-Events/">
+              со спецификацией W3C
+            </a>{' '}
+            {` `}, поэтому не волнуйтесь о кроссбраузерности. События React
+            работают не совсем как нативные. Изучите руководство о
+            SyntheticEvent, чтобы узнать о них больше.
+          </p>
+          <p className="component_text">
+            При использовании React обычно не нужно вызывать addEventListener,
+            чтобы добавить обработчики в DOM-элемент после его создания. Вместо
+            этого добавьте обработчик сразу после того, как элемент
+            отрендерился.
+          </p>
+          <p className="component_text">
+            В компоненте, определённом с помощью ES6-класса, в качестве
+            обработчика события обычно выступает один из методов класса.
+            Например, этот компонент Toggle рендерит кнопку, которая позволяет
+            пользователю переключать состояния между «Включено» и «Выключено»:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'Включено' : 'Выключено'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);`}</code>
+            </pre>
+          </div>
         </div>
       </Content>
     </Layout>
--- src/components/Lifecycle/Lifecycle.jsx
+++ src/components/Lifecycle/Lifecycle.jsx
@@ -9,7 +9,74 @@ const Lifecycle = () => {
     <Layout className="layout">
       <Content style={{ padding: '0 87px' }}>
         <div className="site-layout-content">
-          <h1 className="title">Жизненные циклы</h1>
+          <h1 className="title">
+            <a
+              href="https://ru.reactjs.org/docs/react-component.html#unsafe_componentwillmount"
+              className="color"
+            >
+              {' '}
+              Жизненные циклы
+            </a>
+          </h1>
+          <p className="component_text">
+            Каждый компонент имеет несколько «методов жизненного цикла».
+            Переопределение такого метода позволяет выполнять код на конкретном
+            этапе этого процесса.
+          </p>
+          <h3 className="sub_title">Монтирование</h3>
+          <p className="component_text">
+            При создании экземпляра компонента и его вставке в DOM, следующие
+            методы вызываются в установленном порядке:
+          </p>
+          <ul>
+            <li>{`constructor()`}</li>
+            <li>{`static getDerivedStateFromProps()`}</li>
+            <li>{`render()`}</li>
+            <li>{`componentDidMount()`}</li>
+            <li>{`UNSAFE_componentWillMount()`}</li>
+          </ul>
+
+          <p className="component_text">
+            Метод {`UNSAFE_componentWillMount()`} устарел. Не используйте его в
+            новом коде.
+          </p>
+          <h3 className="sub_title">Обновление</h3>
+          <p className="component_text">
+            Обновление происходит при изменении пропсов или состояния. Следующие
+            методы вызываются в установленном порядке при повторном рендере
+            компонента:
+          </p>
+          <ul>
+            <li>{`static getDerivedStateFromProps()`}</li>
+            <li>{`static getDerivedStateFromProps()`}</li>
+            <li>{`render()`}</li>
+            <li>{`getSnapshotBeforeUpdate()`}</li>
+            <li>{`componentDidUpdate()`}</li>
+            <li>{`UNSAFE_componentWillUpdate()`}</li>
+            <li>{`UNSAFE_componentWillReceiveProps()`}</li>
+          </ul>
+          <p className="component_text">
+            Методы {`UNSAFE_componentWillUpdate()`} и{` `}
+            {`UNSAFE_componentWillReceiveProps()`} устарели. Не используйте их в
+            новом коде.
+          </p>
+          <h3 className="sub_title">Размонтирование</h3>
+          <p className="component_text">
+            Этот метод вызывается при удалении компонента из DOM:
+          </p>
+          <ul>
+            <li>{`componentWillUnmount()`}</li>
+          </ul>
+          <h3 className="sub_title">Обработка ошибок</h3>
+          <p className="component_text">
+            Следующие методы вызываются, если произошла ошибка в процессе
+            рендеринга, методе жизненного цикла или конструкторе любого
+            дочернего компонента.
+          </p>
+          <ul>
+            <li>{`static getDerivedStateFromError()`}</li>
+            <li>{`componentDidCatch()`}</li>
+          </ul>
         </div>
       </Content>
     </Layout>
