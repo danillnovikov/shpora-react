5d0056e6f14d1d7e5849c443350a4172810b0e02
Daniil <danill.novikov@mail.ru>
2022-04-03 00:20:49 +0300

    add finish info

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -10,6 +10,114 @@ const Context = () => {
       <Content style={{ padding: '0 87px' }}>
         <div className="site-layout-content">
           <h1 className="title">Context</h1>
+          <p className="component_text">
+            Контекст позволяет передавать данные через дерево компонентов без
+            необходимости передавать пропсы на промежуточных уровнях.
+          </p>
+          <p className="component_text">
+            Контекст разработан для передачи данных, которые можно назвать
+            «глобальными» для всего дерева React-компонентов (например, текущий
+            аутентифицированный пользователь, UI-тема или выбранный язык).
+          </p>
+          <h2 className="subtitle">React.createContext</h2>
+          <div className="code">
+            <pre>
+              <code>{`const MyContext = React.createContext(defaultValue);`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            Создаёт объект Context. Когда React рендерит компонент, который
+            подписан на этот объект, React получит текущее значение контекста из
+            ближайшего подходящего Provider выше в дереве компонентов.
+          </p>
+          <p className="component_text">
+            Аргумент defaultValue используется только в том случае, если для
+            компонента нет подходящего Provider выше в дереве. Значение по
+            умолчанию может быть полезно для тестирования компонентов в изоляции
+            без необходимости оборачивать их. Обратите внимание: если передать
+            undefined как значение Provider, компоненты, использующие этот
+            контекст, не будут использовать defaultValue.
+          </p>
+          <h2 className="subtitle">Context.Provider</h2>
+          <div className="code">
+            <pre>
+              <code>{`<MyContext.Provider value={/* некоторое значение */}>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            Каждый объект Context используется вместе с Provider компонентом,
+            который позволяет дочерним компонентам, использующим этот контекст,
+            подписаться на его изменения.
+          </p>
+          <p className="component_text">
+            Компонент Provider принимает проп value, который будет передан во
+            все компоненты, использующие этот контекст и являющиеся потомками
+            этого компонента Provider. Один Provider может быть связан с
+            несколькими компонентами, потребляющими контекст. Так же компоненты
+            Provider могут быть вложены друг в друга, переопределяя значение
+            контекста глубже в дереве.
+          </p>
+          <p className="component_text">
+            Все потребители, которые являются потомками Provider, будут повторно
+            рендериться, как только проп value у Provider изменится. Потребитель
+            (включая .contextType и useContext) перерендерится при изменении
+            контекста, даже если его родитель, не использующий данный контекст,
+            блокирует повторные рендеры с помощью shouldComponentUpdate.
+          </p>
+          <h2 className="subtitle">Class.contextType</h2>
+          <div className="code">
+            <pre>
+              <code>{`class MyClass extends React.Component {
+  componentDidMount() {
+    let value = this.context;
+    /* выполнить побочный эффект на этапе монтирования, используя значение MyContext */
+  }
+  componentDidUpdate() {
+    let value = this.context;
+    /* ... */
+  }
+  componentWillUnmount() {
+    let value = this.context;
+    /* ... */
+  }
+  render() {
+    let value = this.context;
+    /* отрендерить что-то, используя значение MyContext */
+  }
+}
+MyClass.contextType = MyContext;`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            В свойство класса contextType может быть назначен объект контекста,
+            созданный с помощью React.createContext(). С помощью этого свойства
+            вы можете использовать ближайшее и актуальное значение указанного
+            контекста при помощи this.context. В этом случае вы получаете доступ
+            к контексту, как во всех методах жизненного цикла, так и в
+            рендер-методе.
+          </p>
+          <h2 className="subtitle">Context.Consumer</h2>
+          <div className="code">
+            <pre>
+              <code>{`<MyContext.Consumer>
+  {value => /* отрендерить что-то, используя значение контекста */}
+</MyContext.Consumer>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            Consumer — это React-компонент, который подписывается на изменения
+            контекста. В свою очередь, использование этого компонента позволяет
+            вам подписаться на контекст в функциональном компоненте.
+          </p>
+          <p className="component_text">
+            Consumer принимает функцию в качестве дочернего компонента. Эта
+            функция принимает текущее значение контекста и возвращает
+            React-компонент. Передаваемый аргумент value будет равен ближайшему
+            (вверх по дереву) значению этого контекста, а именно пропу value
+            компонента Provider. Если такого компонента Provider не существует,
+            аргумент value будет равен значению defaultValue, которое было
+            передано в createContext().
+          </p>
         </div>
       </Content>
     </Layout>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,7 +9,198 @@ const Form = () => {
     <Layout className="layout">
       <Content style={{ padding: '0 87px' }}>
         <div className="site-layout-content">
-          <h1 className="title">Work with Form</h1>
+          <h1 className="title">Формы</h1>
+          <p className="component_text">
+            В React HTML-элементы формы ведут себя немного иначе по сравнению с
+            DOM-элементами, так как у элементов формы изначально есть внутреннее
+            состояние. К примеру, в эту HTML-форму можно ввести имя:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`<form>
+  <label>
+    Имя:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Отправить" />
+</form>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            По умолчанию браузер переходит на другую страницу при отправке
+            HTML-форм, в том числе и этой. Если вас это устраивает, то не надо
+            ничего менять, в React формы работают как обычно. Однако, чаще всего
+            форму удобнее обрабатывать с помощью JavaScript-функции, у которой
+            есть доступ к введённым данным. Стандартный способ реализации такого
+            поведения называется «управляемые компоненты».
+          </p>
+          <h2 className="subtitle">Управляемые компоненты</h2>
+          <p className="component_text">
+            В HTML элементы формы, такие как {`<input>, <textarea> и <select>`},
+            обычно сами управляют своим состоянием и обновляют его когда
+            пользователь вводит данные. В React мутабельное состояние обычно
+            содержится в свойстве компонентов state и обновляется только через
+            вызов {`setState()`}
+          </p>
+          <p className="component_text">
+            Мы можем скомбинировать оба подхода и сделать состояние
+            React-компонента «единственным источником правды». Тогда
+            React-компонент будет рендерить форму и контролировать её поведение
+            в ответ на пользовательский ввод. Значение элемента формы input в
+            этом случае будет контролировать React, а сам элемент будет
+            называться «управляемый компонент».
+          </p>
+          <p className="component_text">
+            Допустим, мы хотим, чтобы предыдущий пример показал в модальном окне
+            введённое имя, когда мы отправляем форму. Тогда можно написать форму
+            в виде управляемого компонента:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Отправленное имя: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Имя:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Отправить" />
+      </form>
+    );
+  }`}</code>
+            </pre>
+          </div>
+          <h2 className="subtitle">Тег textarea</h2>
+          <p className="component_text">
+            HTML-элемент {`<textarea>`} в качестве текста отображает дочерний
+            элемент:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`<textarea>
+  Привет! Тут просто немного текста внутри тега textarea
+</textarea>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            В React {`<textarea>`} использует атрибут value. Таким образом,
+            форму с {`<textarea>`} можно написать почти тем же способом, что и
+            форму с однострочным {`<input>`}:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Будьте любезны, напишите сочинение о вашем любимом DOM-элементе.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Сочинение отправлено: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Сочинение:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Отправить" />
+      </form>
+    );
+  }
+}`}</code>
+            </pre>
+          </div>
+          <h2 className="subtitle">Тег select</h2>
+          <p className="component_text">
+            В HTML {`<select>`} создаёт выпадающий список. HTML-код в этом
+            примере создаёт выпадающий список вкусов:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`<select>
+  <option value="grapefruit">Грейпфрут</option>
+  <option value="lime">Лайм</option>
+  <option selected value="coconut">Кокос</option>
+  <option value="mango">Манго</option>
+</select>`}</code>
+            </pre>
+          </div>
+          <p className="component_text">
+            Пункт списка «Кокос» выбран по умолчанию из-за установленного
+            атрибута selected. React вместо этого атрибута использует value в
+            корневом теге select. В управляемом компоненте так удобнее, потому
+            что обновлять значение нужно только в одном месте (state). Пример:
+          </p>
+          <div className="code">
+            <pre>
+              <code>{`class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Ваш любимый вкус: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Выберите ваш любимый вкус:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Грейпфрут</option>
+            <option value="lime">Лайм</option>
+            <option value="coconut">Кокос</option>
+            <option value="mango">Манго</option>
+          </select>
+        </label>
+        <input type="submit" value="Отправить" />
+      </form>
+    );
+  }
+}`}</code>
+            </pre>
+          </div>
         </div>
       </Content>
     </Layout>
